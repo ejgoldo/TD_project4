5fab1d31f327e17880ce506f83d8299168e89986
EJ Serafin <edgardo@chainblx.com>
2020-02-17 15:20:42 -0800

    added event listener
    added click event listener which starts the game and moves the page from start screen to game screen with the phrase displayed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // code to use constructors in other files
 
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+const startButton = document.getElementById('btn__reset');
+startButton.addEventListener('click', () => {
+    game = new Game();
+    game.startGame();
+});
